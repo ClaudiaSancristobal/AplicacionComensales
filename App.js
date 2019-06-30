238db695d0ae488f8d65363d71c1f791fcc39b3a
ClaudiaSancristobal <claudia.sancristobal@gmail.com>
2019-06-30 20:22:32 -0300

    -m "Cambio"

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+///////////////
+
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, Button, TouchableHighlight, Image } from 'react-native';
 import Icon from '@expo/vector-icons/Ionicons';
